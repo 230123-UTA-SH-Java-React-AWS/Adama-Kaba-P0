792b48a6582ff0066dddb9e4e990be69f41bc4e2
Adama <adamakaba0@gmail.com>
2023-02-09 08:28:44 -0500

    spring api added back end almost complete for connection with progresql database

---
+++ src/main/java/com/revature/SpringBootBackendApplicationTest.java
@@ -0,0 +1,13 @@
+package com.revature.springbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
