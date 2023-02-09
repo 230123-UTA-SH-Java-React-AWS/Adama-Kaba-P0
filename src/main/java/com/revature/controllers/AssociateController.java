f77e3f5f3e9b0d9b0bf67c533c19ea4ea3ee4214
Adama <adamakaba0@gmail.com>
2023-02-09 07:05:35 -0500

    from new IDE upon back end connection

--- src/main/java/com/revature/controllers/AssociateController.java
+++ src/main/java/com/revature/controllers/AssociateController.java
@@ -1,17 +1,8 @@
 package com.revature.controllers;
 
-import com.revature.model.NewUser;
+impor
 
+@Controller
 public class AssociateController {
-    @GetMapping("/signup")
-    public String getSignUp() {
-        return "sign_up";
 
-    }
-
-    @GetMapping("/signin")
-    public String getSignIn() {
-        model.addAttribute("LoginRequest", new NewUser());
-        return "sign_in";
-    }
-}@PostMapping("/register")
+}
--- src/main/java/com/revature/controllers/NewController.java
+++ src/main/java/com/revature/controllers/NewController.java
@@ -1,6 +1,4 @@
 package com.revature.controllers;
 
-@RestController
-public class NewController {
-    @GetMapping()
+
 }
